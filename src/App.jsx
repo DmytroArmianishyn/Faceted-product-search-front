244ac56705f66e9cea2d2e3f3235fa5fb72d2758
Kanen1323 <safari.rre.345@gmail.com>
2026-02-03 23:07:12 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/product" element={<ProductsPage/>} />
+                <Route path="" element={<ProductsPage/>} />
             </Routes>
         </Router>
     );
